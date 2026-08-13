c6e5f89c5355490eb8c3dac02cc6b6369a930616
vipadarapper <vipadarapper@gmail.com>
2026-08-13 12:06:26 +0000

    Fix Toaster JSX so Vercel build succeeds

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
       <body className="antialiased">
         <BootLoader />
         <OfflineBanner />
-        <Toaster>
+        <Toaster
           position="top-center"
           toastOptions={{
             duration: 3000,
